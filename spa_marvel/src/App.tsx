fcbafcbe3091e9600d2b545415b0991fb25deab6
Fabio Costa <fabiocostasilvaoficial@gmail.com>
2023-02-22 00:09:43 -0300

    created fetch, API

--- spa_marvel/src/App.tsx
+++ spa_marvel/src/App.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+import md5 from "md5"
 
 //components
 import Hero from "./components/Hero";
@@ -10,7 +12,32 @@ import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
 
+
+ const ts = Number(new Date());
+ const privateKey = "e404de82f7e2c4f95f89fdeb323754129fbc6228";
+ const publicKey = "1958ce7ec28d260a9708f4547727812a";
+ const hash = md5(ts + privateKey + publicKey);
+
+ //(`https://gateway.marvel.com/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`);
+      
 function App() {
+
+  const [comics, setComics] = useState()
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await fetch(`https://gateway.marvel.com:443/v1/public/comics?ts=${ts}&apikey=${publicKey}&hash=${hash}`);  
+      const data = await response.json();                                                
+      setComics(data);
+     
+    }
+
+    fetchData();
+  }, []);
+
+   useEffect(() =>{
+    comics && console.log(comics);
+   }, [comics])
   return (
     <div className="m-auto antialiased font-sans text-center bg-black text-white">
       <Hero />
